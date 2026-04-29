123507ad814d7192cc8f0ac41469dbe30a330035
Rohan Kumar <rohankumarrr313@gmail.com>
2026-04-29 18:55:29 -0500

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        new = []
+        for num in nums:
+            if num == val:
+                continue
+            new.append(num)
+        
+        for i, num in enumerate(new):
+            nums[i] = num
+        nums = nums[:len(new)]
+
+        return len(new)
